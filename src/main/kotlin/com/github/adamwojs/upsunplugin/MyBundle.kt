de525e06969f69dd2b4786baeedabdbfb3f08679
GitHub Action <action@github.com>
2025-09-01 05:51:15 +0000

    Template cleanup

--- src/main/kotlin/com/github/adamwojs/upsunplugin/MyBundle.kt
+++ src/main/kotlin/com/github/adamwojs/upsunplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.adamwojs.upsunplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/adamwojs/upsunplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/adamwojs/upsunplugin/services/MyProjectService.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.services
+package com.github.adamwojs.upsunplugin.services
 
 import com.intellij.openapi.components.Service
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.adamwojs.upsunplugin.MyBundle
 
 @Service(Service.Level.PROJECT)
 class MyProjectService(project: Project) {
